dba2524e87b7131fe4d079a2b508ee53630f1e99
jeffmikhail <mikhailcruz78@gmail.com>
2024-02-28 00:08:59 +0800

    remove serilog and serilog.sinks.file

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -1,14 +1,7 @@
-using Serilog;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// customized serilog
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
-    .WriteTo.File("log/employeeLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
-
-builder.Host.UseSerilog();
 
 builder.Services.AddControllers(option =>
 {
