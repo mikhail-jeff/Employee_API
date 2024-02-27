e6ebf6f3990a5cc6dbc6c4a05e92064c51ccb0ca
jeffmikhail <mikhailcruz78@gmail.com>
2024-02-28 01:03:14 +0800

    installed EF Core Tools and EF Core SQL Server

--- EmployeeAPI/Models/Employee.cs
+++ EmployeeAPI/Models/Employee.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public string? Job { get; set; }
+        public string? Country { get; set; }
         public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
